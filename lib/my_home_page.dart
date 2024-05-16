442bedadf3bc3873afe4d37eb72d6a391f7ac02a
apichatpunya <apichat.punya@gmail.com>
2024-05-16 17:03:13 +0700

    update

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -8,8 +8,51 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<MyHomePage> {
+  int _counter = 0;
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+
+      if (_counter < 0) {
+        _counter = 0;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter in  Counter'),
+        ),
+        body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(" นับไปแล้ว $_counter ครั้ง"),
+            
+            ElevatedButton(
+                onPressed: _incrementCounter,
+                child: const Icon(Icons.add),
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.green),
+                )),
+            ElevatedButton(
+                onPressed: _decrementCounter,
+                child: const Icon(Icons.remove),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      Color.fromARGB(255, 192, 7, 133)),
+                ))
+          ],
+        )));
   }
 }
